97b9e28b09fbdf946a270ce5f5d610613cefb449
DaFlusher <joyibe5@gmail.com>
2023-12-20 01:13:16 +0100

    updated metatags and links to github

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
         <>
             <footer>
                 <p >
-                    Challenge by Scrimba in collaboration with Kevin Powell <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-                    Coded by <a href="https://github.com/DaFlusher" target="_blank">DaFlusher</a>.
+                    Challenge by Scrimba in collaboration with Kevin Powell <a href="https://www.frontendmentor.io/challenges/space-tourism-multipage-website-gRWj1URZ3/hub" target="_blank">Frontend Mentor</a>. 
+                    Coded by <a href="https://github.com/DaFlusher/FEM-space-tourism" target="_blank">DaFlusher</a>.
                 </p>
             </footer>
         </>
